d7491b6226c19a8326a569bee9a91087d40b3a07
Alexis <alexis.sousa@giffits.de>
2024-05-02 13:26:04 +0100

    feat: :sparkles: Add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Blogs from "./pages/Blogs";
+import Contact from "./pages/Contact";
+import NoPage from "./pages/NoPage";
 
 function App() {
-  return <div className="App">Hello and welcome! welcome my dear!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
